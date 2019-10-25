24d535c85560142798b06bd635f64b6179710516
Jodi Tivis <joditivis@gmail.com>
2019-10-25 16:37:59 -0600

    working on getting the on clicks to work for all crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var playerNum = 0;
 var winCount = 0;
 var lossCount = 0;
 
-    //generates random number from array
+    //generates random number for player to match
     var numToMatch = Math.floor(Math.random() * 120 + 30);
 
     //stores it in console to make sure it's working properly
@@ -19,7 +19,7 @@ var lossCount = 0;
     //resets players guessed number to 0 after player wins or loses
     playerNum = 0;
 
-    // generate a random number for each crystal 
+    // generate a random number between 1-17 for each crystal 
     var crystal1 = Math.floor(Math.random() * 18 + 1);
     var crystal2 = Math.floor(Math.random() * 18 + 1);
     var crystal3 = Math.floor(Math.random() * 18 + 1);
@@ -33,47 +33,75 @@ var lossCount = 0;
     console.log(crystal4);
     console.log(playerNum);
 
+//=========================
+//RESET FUNCTION
+//=========================
+function resetGame() {
+    playerNum = 0;
 
+    var numToMatch = Math.floor(Math.random() * 120 + 30);
+    console.log(numToMatch);
+    $("#random-number").text(numToMatch);
+
+    var crystal1 = Math.floor(Math.random() * 18 + 1);
+    var crystal2 = Math.floor(Math.random() * 18 + 1);
+    var crystal3 = Math.floor(Math.random() * 18 + 1);
+    var crystal4 = Math.floor(Math.random() * 18 + 1);
+
+    $("#number-guessed").text(playerNum);
+
+    console.log(crystal1);
+    console.log(crystal2);
+    console.log(crystal3);
+    console.log(crystal4);
+    console.log(playerNum);
+}
+
+//=========================
+// WIN OR LOSE FUNCTIONS
+//=========================
 function win() {
     winCount++;
     $("#winCount").text(winCount);
-
+    resetGame();
 }
-function loss() {
+function lose() {
     lossCount++;
     $("#lossCount").text(lossCount);
-
+    resetGame();
 }
 
+//==========================
 // ON CLICK FUNCTION
+//==========================
 $("#crystal1").on("click", function(){
-    playerNum += crystal1;
+    playerNum = playerNum + crystal1;
     console.log("Your Number: " + playerNum); 
     $("#number-guessed").text(playerNum);
 
     if (playerNum === numToMatch) {
-        win()
+        win()       
     }
     else if (playerNum > numToMatch) {
-        loss()
+        lose()       
     }
 })
 
 $("#crystal2").on("click", function(){
-    playerNum += crystal2;
+    playerNum = playerNum + crystal2;
     console.log("Your Number: " + playerNum);
     $("number-guessed").text(playerNum);
 
     if (playerNum === numToMatch) {
-        win()
+        win()       
     }
     else if (playerNum > numToMatch) {
-        loss()
+        lose()      
     }
 })
 
 $("#crystal3").on("click", function(){
-    playerNum += crystal3;
+    playerNum = playerNum + crystal3;
     console.log("Your Number: " + playerNum);
     $("number-guessed").text(playerNum);
 
@@ -81,12 +109,12 @@ $("#crystal3").on("click", function(){
         win()
     }
     else if (playerNum > numToMatch) {
-        loss()
+        lose()
     }
 })
 
 $("#crystal4").on("click", function(){
-    playerNum += crystal4;
+    playerNum = playerNum + crystal4;
     console.log("Your Number: " + playerNum);
     $("number-guessed").text(playerNum);
 
@@ -94,8 +122,10 @@ $("#crystal4").on("click", function(){
         win()
     }
     else if (playerNum > numToMatch) {
-        loss()
+        lose()
     }
 })
+
+resetGame()
 
 });
